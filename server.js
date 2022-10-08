e6247f52beff7716131a5094e46be27374150e7f
Mithilesh999 <contectmithilesh1@gmail.com>
2022-10-09 03:54:32 +0530

    build issue resolved

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-//Install express server
-const express = require('express');
+ //Install express server
+ const express = require('express');
+ const path = require('path');
 
-const app = express();
+ const app = express();
 
-app.use(express.static('./dist/'));
+ // Serve only the static files form the dist directory
+ app.use(express.static(__dirname + '/dist/angular-admin'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
-});
+ app.get('/*', function(req,res) {
+     
+ res.sendFile(path.join(__dirname+'/dist/angular-admin/index.html'));
+ });
 
-app.listen(process.env.PORT || 8080);
+ // Start the app by listening on the default Heroku port
+ app.listen(process.env.PORT || 8080);
